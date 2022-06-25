8566b6eb6b2e71258284adba6a4852ff97909ac2
Георгий Князев <saint_georgy@yahoo.com>
2022-06-25 10:49:06 +0300

    Добавьте файлы проекта.

---
+++ 11 uslovniy operator/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _11_uslovniy_operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputText;
+            char firstsymbol = '(';
+            int lenghtText;
+            int maximumNumberOfConsecutiveCharacters = 0;
+            int numberOfConsecutiveCharacters = 0;
+            Console.WriteLine("Введите последовательность символов ( и )");
+            inputText = Console.ReadLine();
+            lenghtText = inputText.Length;
+            Console.WriteLine(lenghtText);
+                       
+            for (int i = 0; i < lenghtText; i++)
+            {
+                if (inputText[i] == firstsymbol)
+                {
+                    numberOfConsecutiveCharacters++;
+                }
+                else
+                {
+                    numberOfConsecutiveCharacters--;
+                    if (numberOfConsecutiveCharacters < 0)
+                    {
+                        break;
+                    }
+                }
+                if (numberOfConsecutiveCharacters > maximumNumberOfConsecutiveCharacters)
+                {
+                        maximumNumberOfConsecutiveCharacters = numberOfConsecutiveCharacters;
+                }
+            }
+            if (numberOfConsecutiveCharacters == 0)
+            {
+                Console.WriteLine("строка коректна. Максимальная глубина: " + maximumNumberOfConsecutiveCharacters);
+            }
+            else
+            {
+                Console.WriteLine("Строка не коректна");
+            }
+            
+           
+        }
+    }
+}
